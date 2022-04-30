858c9d9d4381d32c136c78ea76dc8a49c817e38d
chad <chadmcintirecloud@gmail.com>
2022-04-29 21:02:06 -0600

    function to take actions and another to sample from minibatches

--- Brain/brain.py
+++ Brain/brain.py
@@ -1,6 +1,7 @@
 from Brain.model import PolicyModel, PredictorModel, TargetModel
 import torch
 from torch.optim.adam import Adam
+from Common.utils import RunningMeanStd
 
 class Brain:
     def __init__(self, **config):
@@ -19,6 +20,47 @@ class Brain:
 
         self.total_trainable_params = list(self.current_policy.parameters()) + list(self.predictor_model.parameters())
         self.optimizer = Adam(self.total_trainable_params, lr=self.config["lr"])
+        
+        self.state_rms = RunningMeanStd(shape=self.obs_shape)  
+        self.int_reward_rms = RunningMeanStd(shape=(1,))
 
+        self.mse_loss = torch.nn.MSELoss()
 
+    def get_actions_and_values(self, state, batch=False):
+        if not batch:
+            state = np.expand_dims(state, 0)
+        state = from_numpy(state).to(self.device)
+        with torch.no_grad():
+            dist, int_value, ext_value, action_prob, = self.current_policy(state)
+            action = dist.sample()
+            log_prob = dist.sample()
+        return action.cpu().numpy(), int_value.cpu().numpy().squeeze(), \
+               ext_value.cpu().numpy().squeeze(), log_prob.cpu().numpy(), \
+               action_prob.cpu().numpy()
 
+   
+   #############
+   #We want to have trajectories on gpu
+   #the yield makes this a generator and so the function will only
+   #be called for each iteration of the for loop, but will not save the 
+   #value in memory
+   #see 
+   # https://stackoverflow.com/questions/231767/what-does-the-yield-keyword-do
+   #for more details
+   ############
+    def choose_mini_batch(self, states, actions, int_returns, ext_returns, advs,                          log_probs, next_states):
+        states = torch.ByteTensor(states).to(self.device)
+        next_states = torch.Tensor(next_states).to(self.device)
+        actions = torch.ByteTensor(actions).to(self.device)
+        advs = torch.Tensor(advs).to(self.device)
+        int_returns = torch.Tensor(int_returns).to(self.device)
+        ext_returns = torch.Tensor(ext_returns).to(self.device)
+        log_probs = torch.Tensor(log_probs).to(self.device)
+
+        #return an an array of random ints equal in shape to the 
+        #of the number of minibatches and the size of minibatches
+        indices = np.random.randint(0, len(states), (self.config["n_mini_bach"],                                    self.mini_batch_size))
+
+        for idx in indicies:
+            yield states[idx], actions[idx], int_returns[idx], \
+            ext_returns[idx], advs[idx], log_probs[idx], next_states[idx]
