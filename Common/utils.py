349b7755b3463dce55c5147698e13681a19a34ed
chad <chadmcintirecloud@gmail.com>
2022-05-05 23:14:12 -0600

    finished all but the play.py, needs a second pass though

--- Common/utils.py
+++ Common/utils.py
@@ -1,16 +1,32 @@
 import numpy as np
 import gym
+from copy import deepcopy
+import cv2
 
 def update_mean_var_count_from_moments(mean, var, count, 
                                        batch_mean, batch_var, batch_count):
+   #change from batch mean to mean
    delta = batch_mean - mean
+
+   #increment the total number of samples
    tot_count = count + batch_count
 
+   #as more batches are taken in the mean is changed less
+   # because tot_count approaches infinity asymptotically
    new_mean = mean + delta * batch_count / tot_count
+
+    
+   #the amount the current variance will contribute to variance calc
    m_a = var * count
+   
+   #the amount the batch variance will contribute to variance calc
    m_b = batch_var * batch_count
+
+   #as we gather more batches, total count will again take over asymptotically
+   #so earlier batches change the variance a lot more 
    M2 = m_a + m_b + np.square(delta) * count * batch_count / tot_count
    new_var = M2 / tot_count
+   new_count = tot_count
 
    return new_mean, new_var, new_count
 
@@ -31,15 +47,31 @@ class RunningMeanStd:
         batch_mean = np.mean(x, axis=0)
         batch_var = np.var(x, axis=0)
         batch_count = x.shape[0]
-        self.update_from_moments(batch_mean, batch_var, batch_count)
+        self.update_from_moments( batch_mean, batch_var, batch_count)
 
     def update_from_moments(self, batch_mean, batch_var, batch_count):
         self.mean, self.var, self.count = update_mean_var_count_from_moments(
         self.mean, self.var, self.count, batch_mean, batch_var, batch_count)
 
+
+def explained_variance(ypred, y):
+    """
+    Computes fraction of variance that ypred explains about y.
+    Returns 1 - Var[y-ypred] / Var[y]
+    interpretation:
+        ev=0 => migh as well have predicted zero
+        ev=1 => perfect prediction
+        ev<0 => worse than just predicting zero
+    """
+    assert y.ndim == 1 and ypred.ndim == 1
+    vary = np.var(y)
+    return np.nan if vary == 0 else 1 - np.var(y - ypred) / vary
+
 def mean_of_list(func):
     def function_wrapper(*args, **kwargs):
         lists = func(*args, **kwargs)
+
+        #why not use np.mean here?
         return [sum(list) / len(list) for list in lists[:-4]] + \
         [explained_variance(lists[-2], lists[-1])]
 
@@ -58,11 +90,14 @@ def make_atari(env_id, max_episode_steps, sticky_action=True, max_and_skip=True)
 
     return env
 
-def StickyActionEnv(gym.Wrapper):
+class StickyActionEnv(gym.Wrapper):
     def __init__(self, env, p=0.25):
+        super(StickyActionEnv, self).__init__(env)
         self.p = p
         self.last_action = 0
 
+    #a value between 0 and 1 is selected if the probability is less
+    #we redo an action
     def step(self, action):
         if np.random.uniform() < self.p:
             action = self.last_action
@@ -70,9 +105,120 @@ def StickyActionEnv(gym.Wrapper):
         self.last_action = action
         return self.env.step(action)
 
+    #Help, why is last action not a random action
+    #How does sticky actions work when compound actions were taken?
     def reset(self):
         self.last_action = 0
         return self.env.reset()
 
 
-        
+    
+class RepeatActionEnv(gym.Wrapper):
+    def __init__(self, env):
+        gym.Wrapper.__init__(self, env)
+        self.successive_frame = np.zeros((2,) + self.env.observation_space.shape, dtype=np.uint8)
+    def reset(self, **kwargs):
+        return self.env.reset(**kwargs)
+
+    def step(self, action):
+        reward, done = 0, False
+        for t in range(4):
+            state, r, done, info = self.env.step(action)
+            if t == 2:
+                self.successive_frame[0] = state
+            elif t == 3:
+                self.successive_frame[1] = state
+            reward += r
+            if done:
+                break
+
+        state = self.successive_frame.max(axis=0)
+        return state, reward, done, info
+
+class MontezumaVisitedRoomEnv(gym.Wrapper):
+    def __init__(self, env, room_address):
+        gym.Wrapper.__init__(self, env)
+        self.room_address = room_address
+        self.visited_rooms = set() # Only stores unique numbers
+
+    def step(self, action):
+        state, reward, done, info = self.env.step(action)
+        #ram = self.unwrapped.ale.getRam()
+        ram = self.unwrapped._get_ram()
+        #ram = self.ale.getRam()
+        assert len(ram) == 128
+        if done:
+            if "episode" not in info:
+                info["episode"] = {}
+            info["episode"].update(visited_room=deepcopy(self.visited_rooms))
+            self.visited_rooms.clear()
+        return state, reward, done, info
+
+    def reset(self):
+        return self.env.reset()
+
+class AddRandomStateToInfoEnv(gym.Wrapper):
+    def __init__(self, env):
+        gym.Wrapper.__init__(self, env)
+        self.rng_at_episode_start = deepcopy(self.unwrapped.np_random)
+
+    def step(self, action):
+        state, reward, done, info = self.env.step(action)
+        if done:
+            if 'episode' not in info:
+                info['episode'] = {}
+            info['episode']['rng_at_episode_start'] = self.rng_at_episode_start
+        return state, reward, done, info
+
+    def reset(self):
+        self.rng_at_episode_start = deepcopy(self.unwrapped.np_random)
+        return self.env.reset()
+
+def stack_states(stacked_frames, state, is_new_episode):
+    frame = preprocessing(state)
+
+    if is_new_episode:
+        stacked_frames = np.stack([frame for _ in range(4)], axis=0)
+    else:
+        stacked_frames = stacked_frames[1:, ...]
+        stacked_frames = np.concatenate([stacked_frames, np.expand_dims(frame, axis=0)], axis=0)
+    return stacked_frames
+
+
+
+
+def preprocessing(img):
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    img = cv2.resize(img, (84, 84), interpolation=cv2.INTER_AREA)
+    return img
+
+class RewardForwardFilter(object):
+    def __init__(self, gamma):
+        self.rewems = None
+        self.gamma = gamma
+
+    def update(self, rews):
+        if self.rewems is None:
+            self.rewems = rews
+        else:
+            self.rewems = self.rewems * self.gamma + rews
+        return self.rewems
+
+def clip_grad_norm_(parameters, norm_type: float = 2.0):
+    """
+    This is the official clip_grad_norm implemented in pytorch but the max_norm part has been removed.
+    https://github.com/pytorch/pytorch/blob/52f2db752d2b29267da356a06ca91e10cd732dbc/torch/nn/utils/clip_grad.py#L9
+    """
+    if isinstance(parameters, torch.Tensor):
+        parameters = [parameters]
+    parameters = [p for p in parameters if p.grad is not None]
+    norm_type = float(norm_type)
+    if len(parameters) == 0:
+        return torch.tensor(0.)
+    device = parameters[0].grad.device
+    if norm_type == inf:
+        total_norm = max(p.grad.detach().abs().max().to(device) for p in parameters)
+    else:
+        total_norm = torch.norm(torch.stack([torch.norm(p.grad.detach(), norm_type).to(device) for p in parameters]), norm_type)
+
+    return total_norm
--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import gym
 from Brain.brain import Brain
 from Common.logger import Logger
 from Common.runner import Worker
+from torch.multiprocessing import Pipe, Process
+from tqdm import tqdm
+import numpy as np
 
 def run_workers(worker, conn):
     worker.step(conn)
@@ -60,7 +63,7 @@ if __name__ == '__main__':
         if config["train_from_scratch"]:
             print("---Pre_normalization started.---")
             states = []
-            total_pre_normalization_steps = config["roll_length"] * config["pre_normalization_steps"]
+            total_pre_normalization_steps = config["rollout_length"] * config["pre_normalization_steps"]
             actions = np.random.randint(0, config["n_actions"], (total_pre_normalization_steps, config["n_workers"]))
             for t in range(total_pre_normalization_steps):
 
@@ -83,4 +86,84 @@ if __name__ == '__main__':
         rollout_base_shape = config["n_workers"], config["rollout_length"]
 
         init_states = np.zeros(rollout_base_shape + config["state_shape"], dtype=np.uint8)
+        init_actions = np.zeros(rollout_base_shape, dtype=np.uint8)
+        init_action_probs = np.zeros(rollout_base_shape + (config["n_actions"],))
+        init_int_rewards = np.zeros(rollout_base_shape)
+        init_ext_rewards = np.zeros(rollout_base_shape)
+        init_dones = np.zeros(rollout_base_shape, dtype=np.bool)
+        init_int_values = np.zeros(rollout_base_shape)
+        init_ext_values = np.zeros(rollout_base_shape)
+        init_log_probs = np.zeros(rollout_base_shape)
+        init_next_states = np.zeros((rollout_base_shape[0],) + config["state_shape"], dtype=np.uint8)
+        init_next_obs = np.zeros(rollout_base_shape + config["obs_shape"], dtype=np.uint8)
+        
 
+        logger.on()
+        episode_ext_reward = 0
+        concatenate = np.concatenate
+        for iteration in tqdm(range(init_iteration + 1, config["total_rollouts_per_env"] + 1)):
+            total_states = init_states
+            total_actions = init_actions
+            total_action_probs = init_action_probs
+            total_int_rewards = init_int_rewards
+            total_ext_rewards = init_ext_rewards
+            total_dones = init_dones
+            total_int_values = init_int_values
+            total_ext_values = init_ext_values
+            total_log_probs = init_log_probs
+            next_states = init_next_states
+            total_next_obs = init_next_obs
+            
+            for t in range(config["rollout_length"]):
+                for worker_id, parent in enumerate(parents):
+                    total_states[worker_id, t] = parent.recv()
+
+                total_actions[:, t], total_int_values[:, t], total_ext_values[:, t], total_log_probs[:, t], \
+                total_action_probs[:, t] = brain.get_actions_and_values(total_states[:, t], batch=True)
+                for parent, a in zip(parents, total_actions[:, t]):
+                    parent.send(a)
+
+                infos = []
+                for worker_id, parent in enumerate(parents):
+                    s_, r, d, info = parent.recv()
+                    infos.append(info)
+                    total_ext_rewards[worker_id, t] = r
+                    total_dones[worker_id, t] = d
+                    next_states[worker_id] = s_
+                    total_next_obs[worker_id, t] = s_[-1, ...]
+                
+                episode_ext_reward += total_ext_rewards[0, t]
+                if total_dones[0, t]:
+                    episode += 1
+                    if "episode" in infos[0]:
+                        visited_rooms = infos[0]["episode"]["visited_room"]
+                        logger.log_episode(episode, episode_ext_reward, visited_rooms)
+                    episode_ext_reward = 0
+
+            total_next_obs = concatenate(total_next_obs)
+            total_int_rewards = brain.calculate_int_rewards(total_next_obs)
+            _, next_int_values, next_ext_values, *_ = brain.get_actions_and_values(next_state, batch=True)
+            
+            total_int_rewards = brain.normalize_int_rewards(total_int_rewards)
+
+            training_logs = brain.train(states=concatenate(total_states),
+                                        actions=concatenate(total_actions),
+                                        int_rewards=total_int_rewards,
+                                        ext_rewards=total_ext_rewards,
+                                        dones=total_dones,
+                                        int_values=total_int_values,
+                                        ext_values=total_ext_values,
+                                        log_probs=concatenate(total_log_probs),
+                                        next_int_values=next_int_values,
+                                        next_ext_values=next_ext_values,
+                                        total_next_obs=total_next_obs)
+            
+            logger.log_iteration(iteration,
+                                 training_logs,
+                                 total_int_rewards[0].mean(),
+                                 total_action_probs[0].max(-1).mean())
+
+    else:
+        checkpoint = logger.loads_weights()
+        play = Play(config["env_name"], brain, checkpoint)
+        play.evaluate()
